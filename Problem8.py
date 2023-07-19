b257c19d9fafd55d55e5f4dbabaaa8ccb8acf82f
ThomasCholak <cholakthomas@gmail.com>
2023-07-18 21:12:20 -0500

    push #2 (7/18/2023)

--- Problem8.py
+++ Problem8.py
@@ -1,3 +1,24 @@
 # Thomas Cholak
-
+import difflib
 # Problem 8
+
+from itertools import permutations
+import enchant
+
+d = 2
+
+eng_dict = enchant.Dict("en_US")
+op = set()
+
+inp = input("Please enter a string: ")
+# inp = "extreme"
+lettr = [x.lower() for x in inp]
+
+for n in range(d+1, len(inp)):
+    for y in list(permutations(lettr, d+1)):
+        z = "".join(y)
+        if eng_dict.check(z):
+            op.add(z)
+
+print(f'Possible 3-letter words are:\n  {op}')
+
